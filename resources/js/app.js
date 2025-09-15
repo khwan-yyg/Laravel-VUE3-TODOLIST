93eef8809863f50f882ba1904c2419a41d063595
khwankamon <khwan_yyg@outlook.com>
2025-09-15 15:58:17 +0700

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import "./bootstrap";
+import { createApp } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+createApp(HelloWorld).mount("#app");
---
+++ resources/js/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>Hello from Vue 3!</h1>
+    </div>
+</template>
+
+<script setup></script>
